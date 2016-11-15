ca708174ccc0cc63cccf0c8c572d2182f5cfde1e
Bernard Maisel <ntjust26@gmail.com>
2016-11-15 10:38:36 +0900

    Added Rock paper scissors game

---
+++ course_101/lesson 2 - small programs/rps.rb
@@ -0,0 +1,60 @@
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+def test_method
+  prompt("Testing!")
+end
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'rock' && second == 'lizard') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'paper') ||
+  (first == 'scissors' && second == 'lizard') ||
+    (first == 'spock' && second == 'rock') ||    
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'lizard' && second == 'spock') ||
+  (first == 'lizard' && second == 'paper')
+end
+
+test_method
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing!")
---
+++ course_101/lesson 2 - small programs/rps_bonus.rb
@@ -0,0 +1,118 @@
+VALID_CHOICES = %w(r p s l sp)
+
+CHOICE_NAMES = { "r" => "Rock",
+                 "p" => "Paper",
+                 "s" => "Scissors",
+                 "l" => "Lizard",
+                 "sp" => "Spock" }
+
+COMBOS = {
+  'r' => %w(s l),
+  'p' => %w(r sp),
+  's' => %w(p l),
+  'sp' => %w(r s),
+  'l' => %w(sp p)
+}
+
+def test_method
+  prompt("Testing!")
+end
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+   COMBOS[first].include?(second)
+end
+
+test_method
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+    prompt("You've managed to beat the computer. Congratulations!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def score_counter(player_score, computer_score)
+  if player_score > computer_score
+    prompt("You are winning! Player: #{player_score} **** Computer: #{computer_score}")
+  elsif computer_score > player_score
+    prompt("You are losing! Player: #{player_score} **** Computer: #{computer_score}")
+  else
+    prompt("You are tied! Player: #{player_score} **** Computer: #{computer_score}")
+  end
+end
+
+player_score = 0
+computer_score = 0
+
+loop do
+  choice = ''
+  loop do
+      choice_prompt = <<-MSG
+  Make a choice:
+  r  ----> Rock
+  p  ----> Paper
+  s  ----> Scissors
+  l  ----> Lizard
+  sp ----> Spock
+MSG
+
+  prompt(choice_prompt)
+
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{CHOICE_NAMES[choice]} **** Computer chose: #{CHOICE_NAMES[computer_choice]}")
+
+  
+  if win?(choice, computer_choice)
+    player_score += 1
+    score_counter(player_score, computer_score)
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+    score_counter(player_score, computer_score)
+  else
+    score_counter(player_score, computer_score)
+  end
+
+  if player_score == 5
+display_results(choice, computer_choice)
+    elsif computer_score == 5
+display_results(choice, computer_choice)
+      else player_score == 5 && computer_score == 5
+display_results(choice, computer_choice)
+  end
+#  prompt("Do you want to play again?")
+#  answer = Kernel.gets().chomp()
+#  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing!")
+
+
+#
+#break if player_score == 5
+#  prompt("You've managed to beat the computer. Congratulations!")
+#  
+#elsif computer_score == 5
+#  prompt("The computer has completely overpowered you.")
+#  break
+#elsif player_score == computer_score
+#  prompt("Good luck next time!")
+#  break
+#end
