8ff0f1b19078a7db38888f90d2cd00c265c3e8a5
Bernard Maisel <ntjust26@gmail.com>
2016-12-04 16:38:47 +0900

    ttt.rb with bonus features

---
+++ course_101/lesson_4/ttt_bonus.rb
@@ -0,0 +1,214 @@
+require 'pry'
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]] # diagonals
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+def display_board(brd)
+  system 'cls'
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
+  puts "     |     |"
+  puts ""
+end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square: #{joinor(empty_squares(brd), ', ')}"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice"
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def find_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  end
+end
+
+def key_square(brd)
+  return nil unless brd[5] == INITIAL_MARKER
+  5
+end
+
+def computer_places_piece!(brd)
+  square = nil
+
+  # chooses offensively
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_risk_square(line, brd, COMPUTER_MARKER)
+      break if square
+    end
+  end
+
+  # chooses defensively
+  WINNING_LINES.each do |line|
+    square = find_risk_square(line, brd, PLAYER_MARKER)
+    break if square
+  end
+
+  unless square
+    square = key_square(brd)
+  end
+
+  # picks random square
+  if !square
+    square = empty_squares(brd).sample
+  end
+
+  brd[square] = COMPUTER_MARKER
+end
+
+def who_first
+  answer = ''
+  loop do
+    prompt "Should player or computer go first? ('p' for player, 'c' for computer):"
+    answer = gets.chomp
+    break if answer.downcase == "p" || answer.downcase == "c"
+    prompt "Please input p or c."
+  end
+
+  if answer.downcase == "p"
+    "Player"
+  else
+    "Computer"
+  end
+end
+
+def alternate_player(current_player)
+  if current_player == "Player"
+    "Computer"
+  else
+    "Player"
+  end
+end
+
+def place_piece!(brd, current_player)
+  if current_player == "Player"
+    player_places_piece!(brd)
+  else
+    computer_places_piece!(brd)
+  end
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+player_score = 0
+computer_score = 0
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+def show_results(player, computer)
+  if player > computer
+    prompt "You are winning #{player}:#{computer}!"
+  elsif player < computer
+    prompt "You are losing #{player}:#{computer}!"
+  else prompt "You are tied #{player}:#{computer}!"
+  end
+end
+
+def valid_answer?
+  answer = nil
+  loop do
+    prompt "Play again? (y or n)"
+    answer = gets.chomp.downcase
+    break if answer == "y" || answer == "n"
+    prompt "Please input a valid response."
+  end
+  answer
+end
+
+loop do
+  board = initialize_board
+  display_board(board)
+  current_player = who_first
+
+  loop do
+    loop do
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      display_board(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+    display_board(board)
+
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+    else
+      prompt "It's a tie!"
+    end
+
+    if detect_winner(board) == "Player"
+      player_score += 1
+    elsif detect_winner(board) == "Computer"
+      computer_score += 1
+    end
+    break if player_score == 5 || computer_score == 5
+    show_results(player_score, computer_score)
+  end
+
+  break unless valid_answer? == "y"
+end
+
+prompt "Tanks for playing Tic Tac Toe! Bye-Bye!"
