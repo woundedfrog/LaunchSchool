9285646adae5c02298200c13ceb402ea85263662
Bernard Maisel <ntjust26@gmail.com>
2017-03-27 22:18:28 +0900

    running total - update

--- course_exercises/ruby_109_exercises/easy_4/running_total_further.rb
+++ course_exercises/ruby_109_exercises/easy_4/running_total_further.rb
@@ -1,12 +1,22 @@
 def running_total(array)
   total = 0
   new_a = []
-  array.each do |num, i|
+  array.each do |num|
     total += num
     new_a << total
   end
   new_a
 end
+
+#OR:
+
+#def running_total(array)
+#  new_arr = []
+#  array.size.times do |num|
+#    new_arr << array[0..num].inject(:+)
+#  end
+#  new_arr
+#end
 
 puts running_total([2, 5, 13]) == [2, 7, 20]
 puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
