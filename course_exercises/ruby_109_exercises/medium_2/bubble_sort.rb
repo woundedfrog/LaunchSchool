e1c42c2d1d0acabcd8362d9250a613e3f2056614
Bernard Maisel <ntjust26@gmail.com>
2017-04-12 11:45:22 +0900

    Medium 2 exercises

---
+++ course_exercises/ruby_109_exercises/medium_2/bubble_sort.rb
@@ -0,0 +1,25 @@
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      next if array[index - 1] <= array[index]
+      array[index - 1], array[index] = array[index], array[index - 1]
+      swapped = true
+    end
+    break unless swapped
+  end
+  nil
+
+end
+
+array = [5, 3]
+bubble_sort!(array)
+array == [3, 5]
+
+array = [6, 2, 7, 1, 4]
+bubble_sort!(array)
+array == [1, 2, 4, 6, 7]
+
+array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
+bubble_sort!(array)
+array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
---
+++ course_exercises/ruby_109_exercises/medium_2/sum_square_sum.rb
@@ -0,0 +1,17 @@
+def sum_square_difference(num)
+  sum = 0
+  sum_of_squares = 0
+
+  1.upto(num) do |val|
+    sum += val
+    sum_of_squares += val**2
+  end
+
+  sum**2 - sum_of_squares
+end
+
+p sum_square_difference(3) == 22
+# -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+p sum_square_difference(10) == 2640
+p sum_square_difference(1) == 0
+p sum_square_difference(100) == 25164150
