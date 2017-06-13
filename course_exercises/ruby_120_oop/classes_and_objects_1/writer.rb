d63cf4b8d77ab9ceeb31af9fba4caed5616907f8
Bernard Maisel <ntjust26@gmail.com>
2017-06-13 12:15:50 +0900

    oop basic - classes, objects 1

---
+++ course_exercises/ruby_120_oop/classes_and_objects_1/writer.rb
@@ -0,0 +1,22 @@
+#Writer
+#
+#Using the code from the previous exercise, add a setter method named #name. Then, rename kitty to 'Luna' and invoke #greet again.
+#
+#Code:
+
+class Cat
+  attr_reader :name
+  attr_writer :name
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.name = "Luna"
+kitty.greet
