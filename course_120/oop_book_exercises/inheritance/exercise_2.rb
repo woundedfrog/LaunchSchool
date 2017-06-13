18b9d280157c6b4bba14a344b1a81cc031f972fe
Bernard Maisel <ntjust26@gmail.com>
2017-06-13 11:55:34 +0900

    exercise_2 fixed

--- course_120/oop_book_exercises/inheritance/exercise_2.rb
+++ course_120/oop_book_exercises/inheritance/exercise_2.rb
@@ -6,33 +6,24 @@
 
 
 class Vehicle
-  @@number_of_vehicles = 0
-
-  def self.number_of_vehicles
-    puts "This program has created #{@@number_of_vehicles} vehicles"
-  end
-
-  def initialize
-    @@number_of_vehicles += 1
-  end
-
-  def self.mileage(gallons,miles)
-    puts "#{miles / gallons} miles per gallon of gas"    
-  end
-end
-
-class MyCar < Vehicle
-  NUMBER_OF_DOORS = 4
-  CAR_USE = "Show car"
-
   attr_accessor :color
   attr_reader :year
+  @@number_of_vehicles = 0
 
   def initialize(year,color,model)
     @year = year
     @color = color
     @model = model
     @current_speed = 0
+    @@number_of_vehicles += 1
+  end
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
+
+  def self.mileage(gallons,miles)
+    puts "#{miles / gallons} miles per gallon of gas"    
   end
 
   def speed_up(number)
@@ -60,8 +51,14 @@ class MyCar < Vehicle
   end
 
   def to_s
-    puts "The car is a #{@color} #{@year} #{@model}."  
+    puts "The vehicle is a #{@color} #{@year} #{@model}."  
   end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+  CAR_USE = "Show car"
+
 end
 
 class MyTruck < Vehicle
@@ -71,4 +68,5 @@ end
 
 honda = MyCar.new(2017,"blue","Civic Type-R")
 toyota = MyCar.new(2000,"orange","Toyota Supra")
-MyCar.number_of_vehicles
+chevy = MyTruck.new(2010,"brown","Chevy Bolt")
+puts Vehicle.number_of_vehicles
