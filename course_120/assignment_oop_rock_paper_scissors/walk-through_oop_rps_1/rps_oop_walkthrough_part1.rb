f314275791340b09921363d9e36ac9db75fd2605
Bernard Maisel <ntjust26@gmail.com>
2017-06-14 17:13:23 +0900

    RPS OOP walk-through 1

--- course_120/assignment_oop_rock_paper_scissors/walk-through_oop_rps_1/rps_oop_walkthrough_part1.rb
+++ course_120/assignment_oop_rock_paper_scissors/walk-through_oop_rps_1/rps_oop_walkthrough_part1.rb
@@ -30,19 +30,91 @@ def compare(move1, move2)
 
 end
 
+class Player
+  attr_accessor :move
+
+  def initialize(player_type = :human)  #we have a default value set to human incase nothing is passed in.
+    @player_type = player_type
+    @move = nil
+  end
+
+  def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, scissors:"
+        choice = gets.chomp.downcase
+        break if  ['rock','paper','scissors'].include?(choice)
+        puts "Sorry, invalid choice!"
+      end
+      self.move = choice
+    else
+      self.move = ['rock','paper','scissors'].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
     @human = Player.new
-    @computer = Player.new
+    @computer = Player.new(:computer)  #When instantiating a new object(which is assigned to the instance variable) we pass in the computer as an argument to the player class.
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock Paper Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thank you for playing Rock paper Scissors!"
+  end
+
+  def display_winner
+    puts "You chose #{human.move}."
+    puts "The computer chose #{computer.move}."
+
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts "You won!" if computer.move == 'scissors'
+      puts "Computer won!" if computer.move == 'paper'
+    when 'paper' 
+      puts "It's a tie!" if computer.move == 'paper'
+      puts "You won!" if computer.move == 'rock'
+      puts "Computer won!" if computer.move == 'scissors'
+    when 'scissors'
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts "You won!" if computer.move == 'paper'
+      puts "Computer won!" if computer.move == 'rock'
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y','n'].include?(answer.downcase)
+      puts "Sorry, input must be 'y' or 'n'"
+    end
+    return true if answer == 'y'
+    return false
   end
 
   def play
     display_welcome_message
-    human_choose_move
-    computer_choose_move
-    display_winner
+
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
