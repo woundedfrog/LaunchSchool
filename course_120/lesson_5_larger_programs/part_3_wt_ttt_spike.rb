2e24280ffdbb7b0549a2f551c9fee923b13ab5f9
Bernard Maisel <ntjust26@gmail.com>
2017-07-01 16:41:45 +0900

    ttt walkthrough spike part 2

--- course_120/lesson_5_larger_programs/part_3_wt_ttt_spike.rb
+++ course_120/lesson_5_larger_programs/part_3_wt_ttt_spike.rb
@@ -1,10 +1,23 @@
 class Board
+  INITIAL_MARKER = " "
   def initialize
+    #@squares = Hash.new(Square.new(INITIAL_MARKER))
+    @squares = {}
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+  end
+
+  def get_square_at(key)
+    @squares[key] # returns a square object    
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
@@ -17,6 +30,11 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!\n"
   end
@@ -28,16 +46,16 @@ class TTTGame
   def display_board
     puts ""
     puts "     |     |"
-    puts "     |     |"
-    puts "     |     |"
-    puts "-----+-----+------"
-    puts "     |     |"
-    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
     puts "     |     |"
     puts "-----+-----+------"
     puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
     puts "     |     |"
+    puts "-----+-----+------"
     puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts "     |     |  "
     puts ""
   end
 
@@ -45,13 +63,14 @@ class TTTGame
     display_welcome_message
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+    #display_result
     display_goodbye_message
   end
 end
