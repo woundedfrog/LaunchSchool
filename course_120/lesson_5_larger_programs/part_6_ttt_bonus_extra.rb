4391b220d84c45b0afcefb194569400258a4ef69
Bernard Maisel <ntjust26@gmail.com>
2017-07-08 17:42:34 +0900

    updates
    rearranging methods, changing methods and variables.
    Fixed minor bugs

--- course_120/lesson_5_larger_programs/part_6_ttt_bonus_extra.rb
+++ course_120/lesson_5_larger_programs/part_6_ttt_bonus_extra.rb
@@ -72,9 +72,9 @@ module Displayable
     clear_screen_and_display_board
     case board.winning_marker
     when player1.marker
-      bannerize("Player 1 won!")
+      bannerize("Player 1: #{player1.name} won!")
     when player2.marker
-      bannerize("Player 2 won!")
+      bannerize("Player 2: #{player2.name} won!")
     else
       bannerize("It's a tie!")
     end
@@ -98,37 +98,14 @@ end
 class Board
   include Displayable
 
-  attr_reader :board_size
+  attr_reader :board_size, :win_score
 
   def initialize
     @squares = {}
-    @square_total, @board_size = choose_board_size
+    @squares_total, @board_size = choose_board_size
     @winning_lines = winning_lines
+    @win_score = win_score
     reset
-  end
-
-  def draw_squares(lines, sqr_num)
-    square_row = []
-    sqr_num.upto(lines) do |num|
-      square_row << "  #{@squares[num]}  "
-    end
-    puts square_row.join("|").center(80)
-  end
-
-  def draw_verticals(lines)
-    row = []
-    lines.times do
-      row << "     "
-    end
-    puts row.join("|").center(80)
-  end
-
-  def draw_border_line(lines)
-    row = []
-    lines.times do
-      row << "-----"
-    end
-    puts row.join("+").center(80)
   end
 
   def draw
@@ -156,11 +133,94 @@ class Board
     @squares.keys.select { |key| @squares[key].unmarked? }
   end
 
+  def winning_lines
+    size = board_size
+    sq_nums = (1..size**2).to_a
+    rows = calculate_win_rows(sq_nums, size)
+    cols = calculate_win_cols(rows)
+    diagonals = calculate_win_diagonals(rows, size)
+    rows + cols + diagonals
+  end
+
+  def win_score
+    case board_size
+    when 3 then 3
+    when 5 then 4
+    when 9 then 5
+    end
+  end
+
+  def risked_square(pl_marker)
+    risk_line(pl_marker)
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
+
+  def someone_won?
+    !!winning_marker
+  end
+
+  def winning_marker
+    @winning_lines.each do |line|
+      squares = @squares.values_at(*line)
+      if !!bingo_line?(squares)
+        return return_marker(squares)
+      end
+    end
+    nil
+  end
+
+  def reset
+    (1..@squares_total).each { |key| @squares[key] = Square.new }
+  end
+
+  private
+
+  def choose_board_size
+    answer = nil
+    puts "Choose a board size: (1, 2, or 3)".yellow
+    puts "(1): 3x3 - matches needed => 3"
+    puts "(2): 5x5 - matches needed => 4"
+    puts "(3): 9x9 - matches needed => 5"
+    loop do
+      answer = gets.chomp.to_i
+      break if [1, 2, 3].include?(answer)
+      puts "That's not a valid option!"
+    end
+    grids = [[nil], [9, 3], [25, 5], [81, 9]]
+    grids[answer]
+  end
+
+  def draw_squares(lines, sqr_num)
+    square_row = []
+    sqr_num.upto(lines) do |num|
+      square_row << "  #{@squares[num]}  "
+    end
+    puts square_row.join("|").center(80)
+  end
+
+  def draw_verticals(lines)
+    row = []
+    lines.times do
+      row << "     "
+    end
+    puts row.join("|").center(80)
+  end
+
+  def draw_border_line(lines)
+    row = []
+    lines.times do
+      row << "-----"
+    end
+    puts row.join("+").center(80)
+  end
+
   # rubocop:disable Metrics/AbcSize
   def additional_rows(rows, size)
-    additional = []
-      if size == 9
-      additional = (1...5).flat_map do |x|
+    if size == 9
+      (1...5).flat_map do |x|
         [(x...size).map { |i| rows[i][i - x] },
          (x...size).map { |i| rows[i][-i + x - 1] },
          (0...size - x).map { |i| rows[i][i + x] },
@@ -185,112 +245,51 @@ class Board
     rows.first.zip(*rows[1..-1])
   end
 
-  def winning_lines
-    size = @board_size
-    sq_nums = (1..size**2).to_a
-    rows = calculate_win_rows(sq_nums, size)
-    cols = calculate_win_cols(rows)
-    diagonals = calculate_win_diagonals(rows, size)
-    rows + cols + diagonals
-  end
-
-  def win_score
-    case board_size
-    when 3 then 3
-    when 5 then 4
-    when 9 then 5
-    end
-  end
-# checks each_con(depending on board size).
-#  # if there is a match, then it finds the empty square. 
-#      #returns that square UNLESS the square is in an each_con and the markers don't match the needed count.
-#      # e.g. if the line is ['x','x',x',' ',' '] then it will select the 4th place.
-#      #       if the line is [' ','x','x','x','o'] then it will select the 1st place.
-#      #   but if the lines is ['x','x',x','0',' '] then it should not return the 5th square.
-#          #this is because the the last each_con (consisting of 4 spaces) doesn't include 3 matches
-
   def risk_line(pl_marker)
     @winning_lines.each do |line|
       line.each_cons(win_score) do |bingo|
-        @board_size == 3 ? group_size = win_score - 1 : group_size = win_score - 2
-      if bingo.count { |num| @squares[num].marker == pl_marker } >= (group_size)
-        return bingo.find { |key| @squares[key].marker == " " } if @squares.values_at(*bingo).map { |x| x.marker }.include?(" ") &&  confirm_grouping(bingo, pl_marker)
-      end
+        group_size = @board_size == 3 ? win_score - 1 : win_score - 2
+
+        if bingo.count { |num| @squares[num].marker == pl_marker } >= group_size
+          if @squares.values_at(*bingo).map(&:marker).include?(" ") &&
+             confirm_grouping(bingo, pl_marker)
+            return bingo.find { |key| @squares[key].marker == " " }
+          end
+        end
         next
       end
     end
+
     nil
   end
 
   def confirm_grouping(bingo, pl_marker)
     1.upto(win_score) do |i|
-      @squares.values_at(*bingo).map { |x| x.marker }.each_cons(win_score - 1) do |x|
-        return true if x.uniq.size == 2 && x.uniq.include?(" ") && x.uniq.include?(pl_marker) && x.count(pl_marker) == (win_score - i)
-        # Maybe find and return the " " square in here and use/return that square in the riskline method
+      @squares.values_at(*bingo).map(&:marker).each_cons(win_score - 1) do |x|
+        return true if x.uniq.size == 2 && x.uniq.include?(" ") &&
+                       x.uniq.include?(pl_marker) &&
+                       x.count(pl_marker) == (win_score - i)
       end
     end
     nil
-  end
-
-  def risked_square(pl_marker)
-    risk_line(pl_marker)
-  end
-
-  def full?
-    unmarked_keys.empty?
-  end
-
-  def someone_won?
-   return !!winning_marker
-  end
-
-  def winning_marker
-    @winning_lines.each do |line|
-      squares = @squares.values_at(*line)
-      if bingo_line?(squares)
-        return return_marker(squares)
-      end
-    end
-    nil
-  end
-
-  def reset
-    (1..@square_total).each { |key| @squares[key] = Square.new }
-  end
-
-  private
-
-  def choose_board_size
-    answer = nil
-    puts "Choose a board size: (1, 2, or 3)".yellow
-    puts "(1): 3x3 - matches needed => 3"
-    puts "(2): 5x5 - matches needed => 4"
-    puts "(3): 9x9 - matches needed => 5"
-    loop do
-      answer = gets.chomp.to_i
-      break if [1, 2, 3].include?(answer)
-      puts "That's not a valid option!"
-    end
-    grids = [[nil], [9, 3], [25, 5], [81, 9]]
-    grids[answer]
   end
 
   def bingo_line?(squares)
-    markers = squares.select(&:marker).collect(&:marker)
+    markers = squares.map(&:marker)
     markers.any? do |x|
       next if x == " "
       markers.each_cons(win_score) do |row|
-        return true if row.count(x) == win_score 
+        return x if row.count(x) == win_score
       end
     end
     false
   end
 
   def return_marker(squares)
-    markers = squares.select(&:marker).collect(&:marker)
-    markers.uniq.each do |x|
-      return x if markers.count(x) == win_score
-      puts "didn't pass" # this is a debuggin puts. It shows when the above fails.
+    markers = squares.map(&:marker).uniq
+    markers.each do |x|
+      return x if x == bingo_line?(squares)
+      puts "didn't pass" # Debugging PUTS. It shows when the above fails.
     end
     nil
   end
@@ -378,7 +377,7 @@ class Human < Player
   end
 
   def move(players, board)
-    human_player = players[0]
+    human_player = players[0].marker
     bannerize("Choose a square (#{joinor(board.unmarked_keys)}): ")
     square = nil
     loop do
@@ -386,7 +385,7 @@ class Human < Player
       break if board.unmarked_keys.include?(square)
       puts "Sorry not a valid choice."
     end
-    board[square].marker = human_player.marker
+    board[square] = human_player
   end
 end
 
@@ -406,14 +405,14 @@ class Computer < Player
 
   def choose_marker
     loop do
-      choice = ['O', 'V', 'C', 'B', '*'].sample
+      choice = ['O', 'T', 'M', 'B', '*'].sample
       @@used_markers.include?(choice) ? next : @@used_markers << choice
       return choice
     end
   end
 
   def move(players, board)
-    #sleep 1
+    # sleep 1
     player1 = players[0].marker
     players.drop(1).each do |player|
       player2 = player.marker
@@ -424,13 +423,8 @@ class Computer < Player
       return board[square] = player1 if !square.nil?
     end
     # middle if possible, else random
-    case board::board_size
-    when 3 then return board[5] = player1 if board[5].marker == " "
-    when 5 then return board[13] = player1 if board[13].marker == " "
-    when 9 then return board[41] = player1 if board[41].marker == " "
-    end
-    #return board[5] = player1 if board[5].marker == " "
-    board[board.unmarked_keys.sample] = player1
+    middle = choose_center_square(player1, board)
+    board[board.unmarked_keys.sample] = player1 if middle.nil?
   end
 
   def offensive_move(player2, board)
@@ -439,6 +433,15 @@ class Computer < Player
 
   def defensive_move(player1, board)
     board.risked_square(player1)
+  end
+
+  def choose_center_square(player1, board)
+    size = board.instance_variable_get(:@board_size)
+    case size
+    when 3 then board[5] = player1 if board[5].marker == " "
+    when 5 then board[13] = player1 if board[13].marker == " "
+    else board[41] = player1 if board[41].marker == " "
+    end
   end
 end
 
@@ -488,7 +491,7 @@ class TTTGame
       puts "(1): 2 Players: human & computer"
       puts "(2): 2 Players: human & human"
       puts "(3): 2 Players: computer & computer"
-      puts "(4): 2 Players: human, human, & computer"
+      puts "(4): 3 Players: human, human, & computer"
       answer = gets.chomp.to_i
       break if [1, 2, 3, 4].include?(answer)
       puts "That's not a valid input!"
@@ -497,14 +500,12 @@ class TTTGame
   end
 
   def initialize_players(answer)
-    player =
-      case answer
-      when 1 then [@player1 = Human.new, @player2 = Computer.new]
-      when 2 then [@player1 = Human.new, @player2 = Human.new]
-      when 3 then [@player1 = Computer.new, @player2 = Computer.new]
-      else [@player1 = Human.new, @player2 = Human.new, @player3 = Computer.new]
-      end
-    player
+    case answer
+    when 1 then [@player1 = Human.new, @player2 = Computer.new]
+    when 2 then [@player1 = Human.new, @player2 = Human.new]
+    when 3 then [@player1 = Computer.new, @player2 = Computer.new]
+    else [@player1 = Human.new, @player2 = Human.new, @player3 = Computer.new]
+    end
   end
 
   def current_player_moves
