8ab7347e444aae753b82898005ec10a383bb32ee
Bernard Maisel <ntjust26@gmail.com>
2017-07-19 17:50:21 +0900

    blackjack 21 spike

---
+++ course_120/lesson_5_larger_programs/21_spike.rb
@@ -0,0 +1,74 @@
+class Player
+  def initialize
+    # what would the "data" or "states" of a Player object entail?
+    # maybe cards? a name?
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+    # definitely looks like we need to know about "cards" to produce some total
+  end
+end
+
+class Dealer
+  def initialize
+    # seems like very similar to Player... do we even need this?
+  end
+
+  def deal
+    # does the dealer or the deck deal?
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
+class Participant
+  # what goes in here? all the redundant behaviors from Player and Dealer?
+end
+
+class Deck
+  def initialize
+    # obviously, we need some data structure to keep track of cards
+    # array, hash, something else?
+  end
+
+  def deal
+    # does the dealer or the deck deal?
+  end
+end
+
+class Card
+  def initialize
+    # what are the "states" of a card?
+  end
+end
+
+class Game
+  def start
+    deal_cards
+    show_initial_cards
+    player_turn
+    dealer_turn
+    show_result
+  end
+end
+
+Game.new.start
