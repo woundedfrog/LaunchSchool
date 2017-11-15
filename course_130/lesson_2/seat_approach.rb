1983663d0431125fd4f7892d4d279603855564af
Bernard Maisel <ntjust26@gmail.com>
2017-11-15 13:34:57 +0900

    minitests lectures update

--- course_130/lesson_2/seat_approach.rb
+++ course_130/lesson_2/seat_approach.rb
@@ -3,20 +3,20 @@ require 'minitest/autorun'
 require_relative 'car'
 
 class CarTest < MiniTest::Test
+  def setup
+    @car = Car.new
+  end
 
   def test_car_exists
-    car = Car.new
-    assert(car)
+    assert(@car)
   end
 
   def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+    assert_equal(4, @car.wheels)
   end
 
   def test_name_is_nil
-    car = Car.new
-    assert_nil(car.name)
+    assert_nil(@car.name)
   end
 
   def test_raise_initialize_with_arg
@@ -26,15 +26,13 @@ class CarTest < MiniTest::Test
   end
 
   def assert_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
+    assert_instance_of(Car, @car)
   end
 
   def test_includes_car
-    car = Car.new
     arr = [1, 2, 3]
-    arr << car
+    arr << @car
 
-    assert_includes(arr, car)
+    assert_includes(arr, @car)
   end
 end
