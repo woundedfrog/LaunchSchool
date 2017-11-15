baf2a4bafc2c37d3133c0bcba07d49f674bfbd99
Bernard Maisel <ntjust26@gmail.com>
2017-11-15 10:03:20 +0900

    Added methods
    size / first / last / add

---
+++ course_130/lesson_1/todolist.rb
@@ -0,0 +1,85 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def <<(obj)
+    raise TypeError.new("Can only add Todo objects") unless obj.instance_of?(Todo)
+
+    @todos << obj
+  end
+  alias_method :add, :<<
+
+  def first
+    @todos[0]
+  end
+
+  def last
+    @todos[-1]
+  end
+
+  def size
+    @todos.size
+  end
+end
+
+# given
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+list.size                       # returns 3
+
+# first
+list.first                      # returns todo1, which is the first item in the list
+
+# last
+list.last
