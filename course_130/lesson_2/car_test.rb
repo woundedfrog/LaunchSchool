9c1f86b353bcc449ecd29d744da9c5ad7b3f91bc
Bernard Maisel <ntjust26@gmail.com>
2017-11-15 13:31:24 +0900

    Minitests lectures

--- course_130/lesson_2/car_test.rb
+++ course_130/lesson_2/car_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
+#require "minitest/reporters"
+#Minitest::Reporters.use!
 
 require_relative 'car'
 
@@ -9,16 +9,45 @@ class CarTest < MiniTest::Test
     car = Car.new
     assert_equal(4, car.wheels)
   end
-  
+
   def test_class_match
     car = Car.new
-    assert_equal(true, car.instance_of?(Car)) #extra test for practice
+    assert_instance_of(Car, car) #extra test for practice
   end
-  
+
   # test made to fail
   def test_bad_wheels
     skip('Passed for testing')
     car = Car.new
     assert_equal(3, car.wheels)
+  end
+
+  def test_car_exists
+    car = Car.new
+    assert(car)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")         # this code raises ArgumentError, so this assertion passes
+    end
+  end
+
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  def test_includes_car
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+
+    assert_includes(arr, car)
   end
 end
