74963161c0d92f67a718068ede4e537e455f6204
Bernard Maisel <ntjust26@gmail.com>
2018-04-08 13:08:00 +0900

    meetup - final pass
    refactored

--- course_exercises/challenges/medium_1/meetup.rb
+++ course_exercises/challenges/medium_1/meetup.rb
@@ -10,53 +10,30 @@
 # Note that "Monteenth", "Tuesteenth", etc are all made up words. There was a meetup whose members realised that there are exactly 7 days that end in '-teenth'. Therefore, one is guaranteed that each day of the week (Monday, Tuesday, ...) will have exactly one date that is named with '-teenth' in every month.
 #
 # Write code that calculates date of meetups.
-require 'date'
 
-class Date
-  def teenth
-    (13..19).to_a.include?(self.day)
-  end
-
-  # def first(compared_day)
-  #   # self
-  #   # day = Time.new(self.year,self.mon,self.day).strftime("%A")
-  #
-  #   counter = 0
-  #   1.upto(31) do |c|
-  #     new_date = self.next_day(n=c)
-  #     counter += 1 if new_date.day == new_date.compared_day
-  #     return true if counter == 1
-  #   end
-  # end
-
-
-end
 
 class Meetup
   def initialize(month, year)
     @month = month
     @year = year
+    @date = Date.new(@year, @month, 1)
   end
 
   def day(day, placement)
-    @date = Date.new(@year, @month, 1)
-
-    total_days.times do |digit|
+    total_days.times do
       return @date if @date.send((day.to_s + "?").to_sym) == true && method_helper(day, placement)
-      n = 1
-      @date = @date.next_day(n=n)
-      n += 1
+      counter = 1
+      @date = @date.next_day(n=counter)
+      counter += 1
     end
   end
-#i know it's a teen day
-# i know it's a monday
 
   def method_helper(day, placement)
     @day = (day.to_s + "?").to_sym
 
     case placement
     when :teenth
-    return @date.send(placement) == true
+      return send(placement) == true
     when :first
       return send(placement)
     when :second
@@ -72,55 +49,24 @@ class Meetup
     end
   end
 
+  def teenth
+    (13..19).to_a.include?(@date.day)
+  end
 
   def first
-
       day_calculator(1)
-    # new_date = Date.new(@year,@month,1)
-    # counter = 0
-    # 31.times do |c|
-    #   date = new_date.next_day(n=c)
-    #   counter += 1 if date.send(@day)
-    #   counter += 1 if what_day?(new_date) == what_day?(@date)
-    #   return true if counter == 1
-    # end
   end
 
   def second
     day_calculator(2)
-    # new_date = Date.new(@year, @month, 1)
-    #
-    # counter = 0
-    # 31.times do |c|
-    #   date = new_date.next_day(n=c)
-    #   counter += 1 if date.send(@day)
-    #   return date if counter == 2
-    # end
   end
 
   def third
-
       day_calculator(3)
-    # new_date = Date.new(@year, @month, 1)
-    #
-    # counter = 0
-    # 31.times do |c|
-    #   date = new_date.next_day(n=c)
-    #   counter += 1 if date.send(@day)
-    #   return date if counter == 3
-    # end
   end
 
   def fourth
     day_calculator(4)
-    # new_date = Date.new(@year, @month, 1)
-    #
-    # counter = 0
-    # 31.times do |c|
-    #   date = new_date.next_day(n=c)
-    #   counter += 1 if date.send(@day)
-    #   return date if counter == 4
-    # end
   end
 
   def last
@@ -153,6 +99,3 @@ class Meetup
     Time.new(new_date.year,new_date.mon,new_date.day).strftime("%A")
   end
 end
-
-
- p Date.new(2013, 2, 2) == Meetup.new(2, 2013).day(:saturday, :first)
