75498b4771220cb20c97778abdd05b027b3bf94f
Bernard Maisel <ntjust26@gmail.com>
2018-04-08 17:00:03 +0900

    nth_prime

--- course_exercises/challenges/medium_1/nth_prime.rb
+++ course_exercises/challenges/medium_1/nth_prime.rb
@@ -4,7 +4,6 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # Do not use Ruby's Prime class nor any of the prime-number testing methods in the Integer class. For an algorithm to determine whether a number is prime, check out this article.
-require 'pry'
 class Prime
 
   def self.nth(nth)
